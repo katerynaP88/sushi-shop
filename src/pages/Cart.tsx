c3af36fa797df362478ecbaec6ee1aa5d7491585
[Kateryna <potapovakaterina15@gmail.com>
2025-05-28 19:32:40 +0300

    Fix typo in sushiData.json: change 'Salamon Nigiri' to 'Salmon Nigiri'

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -17,7 +17,33 @@ const Cart = () => {
     return (
         <div style={{ padding: "2rem" }}>
             <h1>Your Cart</h1>
-            <p>Items in your cart: {cart.items.length}</p>
+            <div 
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+                gap: "1rem",
+              }}
+        >
+            {cart.items.map((item) => (
+                <div
+                  key={item.id}
+                  style={{
+                    borderRadius: "10px",
+                    padding: "1rem",
+                    backgroundColor: "#fff5ee",
+                  }}
+                >
+                  <img
+                    src={item.thumbnail}
+                    alt={item.title}
+                    style={{ width: "100%", borderRadius: "8px" }}
+                  />
+                  <h3>{item.title}</h3>
+                  <p><strong>Price:</strong> ${item.price}</p>
+                  <p><strong>Quantity:</strong> ${item.quantity}</p>
+                </div>
+            ))}
+            </div>
             <Link to="/menu">Go to Menu</Link>
         </div>
     );
