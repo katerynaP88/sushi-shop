75b9b80ef18bc0020cde3cba96114a686d228be6
[Kateryna <potapovakaterina15@gmail.com>
2025-05-28 18:53:06 +0300

    Add basic Cart page structure with item count and menu link

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,7 +1,17 @@
-function Cart() {
+import { useCart } from "../context/CartContext";
+import { Link } from "react-router-dom";
+
+
+const Cart = () => {
+    const { cart } = useCart();
+
     return (
-        <h1>Your Cart</h1>
+        <div style={{ padding: "2rem" }}>
+            <h1>Your Cart</h1>
+            <p>Items in your cart: {cart.items.length}</p>
+            <Link to="/menu">Go to Menu</Link>
+        </div>
     );
-}
+};
 
 export default Cart;
