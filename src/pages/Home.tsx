5d907e070118ef98787ddc70faad8e1064550859
[Kateryna <potapovakaterina15@gmail.com>
2025-05-29 20:53:32 +0300

    Update Home.tsx sushi background and Start Your Order button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,48 @@
-function Home() {
+import { Link } from "react-router-dom";
+import sushiBg from "../assets/sushi-bg.jpg";
+
+
+const Home = () => {
     return (
-        <h1>Welcome to Sushi Shop</h1>
+        <div
+          style={{
+            backgroundImage: `url(${sushiBg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundColor: "#000",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "#fff",
+            textAlign: "center",
+            padding: "1rem",            
+          }}
+        >
+            <img src={sushiBg} alt="Debug sushi background" style={{ width: "100px" }} />
+          <h1 style={{ fontSize: "3rem", marginBottom: "1rem" }}>
+            Welcome to Sushi Shop
+          </h1>
+          <p style={{ fontSize: "1.5rem", marginBottom: "2rem" }}>
+            Discover the freshest sushi made just for you!
+          </p>
+          <Link to="/menu">
+            <button
+             style={{
+                padding: "1rem 2rem",
+                backgroundColor: "#f28c38",
+                color: "#fff",
+                border: "none",
+                borderRadius: "8px",
+                fontSize: "1.2rem",
+                cursor: "pointer",
+              }}
+            >
+              Start Your Order
+            </button>
+          </Link>
+        </div>
     );
 }
 
