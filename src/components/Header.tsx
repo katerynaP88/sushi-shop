26349dc4af61fbb4c33b3efd585fdea1420e968b
[Kateryna <potapovakaterina15@gmail.com>
2025-05-29 22:36:20 +0300

    Remove Cart link from Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,8 +9,7 @@ function Header() {
         <header style={{ padding: "1rem", borderBottom: "1px solid #ccc"}}>
             <nav style={{ display: "flex", gap: "1rem"}}>
                 <Link to="/">Home</Link>
-                <Link to="/menu">Menu</Link>
-                <Link to="/cart">Cart ({itemCount})</Link>
+                <Link to="/menu">Menu</Link>                
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
             </nav>
