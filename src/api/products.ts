af2533672074baef65cdebd79da6cbe932403dd1
[Kateryna <potapovakaterina15@gmail.com>
2025-05-30 23:26:26 +0300

    Update products.ts

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,21 +1,21 @@
 export async function fetchProducts() {
     try {
-        const response = await fetch('https://dummyjson.com/products');
-        if (!response.ok) throw new Error('Failed to fetch products');
+        const response = await fetch("https://dummyjson.com/products");
+        if (!response.ok) throw new Error("Failed to fetch products");
         const data = await response.json();
         return data.products;
     } catch (error) {
-        throw new Error('Failed to load product prices');
+        throw new Error('Error fetching products');
     }
 }
 
 export async function fetchProductById(id: string) {
     try {
-        const response = await fetch('https://dummyjson.com/products/${id}');
-        if (!response.ok) throw new Error('Failed to fetch product');
+        const response = await fetch(`https://dummyjson.com/products/${id}`);
+        if (!response.ok) throw new Error("Failed to fetch product");
         const data = await response.json();
         return data;
     } catch (error) {
-        throw new Error('Failed to load product price');
+        throw new Error("Error fetchung product");
     }
 }
